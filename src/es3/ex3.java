05d0bcf503ce7b6ed90c7e439f678472429c3bb3
alessandraciccone <alessandraciccone@icloud.com>
2025-09-09 17:46:35 +0200

    .

--- src/es3/ex3.java
+++ src/es3/ex3.java
@@ -6,23 +6,23 @@ public class ex3 {
 
     public static void esercizioWhile() {
         Scanner scanner = new Scanner(System.in);
-        String input;
+        String input = "";
 
-        do {
+        System.out.print("Inserisci una stringa (\"-q\" per uscire): ");
+        input = scanner.nextLine();
+
+        while (!input.equals("-q")) {
+            String[] caratteri = input.split("");
+
+            String risultato = String.join(", ", caratteri);
+
+            System.out.println(risultato);
+
             System.out.print("Inserisci una stringa (\"-q\" per uscire): ");
             input = scanner.nextLine();
+        }
 
-            if (!input.equals("-q")) {
-                for (int i = 0; i < input.length(); i++) {
-                    System.out.print(input.charAt(i));
-                    if (i < input.length() - 1) {
-                        System.out.print(", ");
-                    }
-                }
-                System.out.println();
-            }
-
-        } while (!input.equals("-q"));
+        System.out.println("Uscita dal programma!");
     }
 
     public static void main(String[] args) {
